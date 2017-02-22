e63fc133a81b5d23fdcd35d9bd6f52ef5ef1853c
Uriel Salischiker <uriel@urielsalis.me>
2017-02-22 12:06:44 -0300

    Initial commit

---
+++ app/src/main/java/me/urielsalis/swipesnap/MainActivity.java
@@ -0,0 +1,117 @@
+package me.urielsalis.swipesnap;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    GalleryAdapter mAdapter;
+    RecyclerView mRecyclerView;
+
+    ArrayList<ImageModel> data = new ArrayList<>();
+    private ArrayList<String> fileList = new ArrayList<>();
+
+    public ArrayList<String> getFile(File dir) {
+        File listFile[] = dir.listFiles();
+        if (listFile != null && listFile.length > 0) {
+            for (File file : listFile) {
+                if (file.isDirectory()) {
+                    getFile(file);
+                }
+                else {
+                    if (file.getName().endsWith(".png")
+                            || file.getName().endsWith(".jpg")
+                            || file.getName().endsWith(".jpeg")
+                            || file.getName().endsWith(".gif")
+                            || file.getName().endsWith(".bmp")
+                            || file.getName().endsWith(".webp"))
+                    {
+                        String temp = file.getPath();
+                        if (!fileList.contains(temp))
+                            fileList.add(temp);
+                    }
+                }
+            }
+        }
+        return fileList;
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 44555);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 44556);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 44557);
+            }
+        }
+
+
+        File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        getFile(root);
+        for (int i = 0; i < fileList.size(); i++) {
+            String stringFile = fileList.get(i);
+            ImageModel imageModel = new ImageModel();
+            imageModel.setName("Image " + i);
+            imageModel.setUrl(stringFile);
+            data.add(imageModel);
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mAdapter = new GalleryAdapter(MainActivity.this, data);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                new RecyclerItemClickListener.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                        intent.putParcelableArrayListExtra("data", data);
+                        intent.putExtra("pos", position);
+                        startActivity(intent);
+
+                    }
+                }));
+
+    }
+
+}
